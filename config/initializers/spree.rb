77fc5c4cfbfcf53536fd5918502168ea1c3376e2
Cliff Rodgers + spinningtopsofdoom <dev+kliph+spinningtopsofdoom@bendyworks.com>
2015-11-06 10:14:14 -0600

    Redirect to taxonomies for Marketers
    - They have that ability so show them the page instead of an
      unauthorized message

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -19,3 +19,15 @@ Spree::Ability.register_ability(Marketer)
 
 
 Spree.user_class = "Spree::User"
+
+Spree::Admin::RootController.class_eval do
+  alias_method :gem_root_redirect_path, :admin_root_redirect_path
+
+  def admin_root_redirect_path
+    if spree_current_user.has_spree_role?('Marketer')
+      admin_taxonomies_path
+    else
+      gem_root_redirect_path
+    end
+  end
+end
